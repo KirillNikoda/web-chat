c88921ba91fc0adebe0b0fb1062777c2296bacac
KirillNikoda <nikodakirill386@gmail.com>
2022-01-02 18:02:07 +0300

    feat: add login form

--- frontend/src/pages/Auth/index.jsx
+++ frontend/src/pages/Auth/index.jsx
@@ -1,9 +1,22 @@
-import { Input } from 'antd';
-import { Block, Button } from 'components';
+import { Block } from 'components';
+import { Form, Input, Button, Checkbox } from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
+
+import { useEffect, useState } from 'react';
 
 import './Auth.scss';
 
 const Auth = () => {
+  const [, forceUpdate] = useState({});
+
+  useEffect(() => {
+    forceUpdate({});
+  }, []);
+
+  const onFinish = (values) => {
+    console.log('Finish:', values);
+  };
+
   return (
     <section className='auth'>
       <div className='auth__content'>
@@ -12,10 +25,60 @@ const Auth = () => {
           <p>Пожалуйста, войдите в свой аккаунт</p>
         </div>
         <Block>
-          <Input placeholder='qwe' />
-          <Button size='large' type='primary'>
-            chat
-          </Button>
+          <Form
+            name='normal_login'
+            className='login-form'
+            initialValues={{
+              remember: true,
+            }}
+            onFinish={onFinish}
+          >
+            <Form.Item
+              hasFeedback
+              name='username'
+              rules={[
+                {
+                  required: true,
+                  message: 'Пожалуйста, введите имя пользователя!',
+                },
+              ]}
+            >
+              <Input
+                prefix={<UserOutlined className='site-form-item-icon' />}
+                placeholder='Username'
+              />
+            </Form.Item>
+            <Form.Item
+              hasFeedback
+              name='password'
+              rules={[
+                {
+                  required: true,
+                  message: 'Пожалуйста, введите пароль!',
+                },
+              ]}
+            >
+              <Input
+                prefix={<LockOutlined className='site-form-item-icon' />}
+                type='password'
+                placeholder='Password'
+              />
+            </Form.Item>
+
+            <Form.Item>
+              <Button
+                type='primary'
+                htmlType='submit'
+                className='button login-form-button'
+                size='large'
+              >
+                Войти в аккаунт
+              </Button>
+            </Form.Item>
+            <a href='#' className='auth__register-link'>
+              Зарегистрироваться
+            </a>
+          </Form>
         </Block>
       </div>
     </section>
