05dc43d4fd6e9f81b09b8e525dec64302a412524
KirillNikoda <nikodakirill386@gmail.com>
2022-01-02 23:42:03 +0300

    fix: delete dist folder

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,11 +1,22 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = __importDefault(require("express"));
-var app = (0, express_1.default)();
-app.get('/', function (req, res) {
-    res.send('hello world ts');
+require("reflect-metadata");
+var dotenv = require("dotenv");
+var bodyParser = require("body-parser");
+var inversify_express_utils_1 = require("inversify-express-utils");
+var inversify_config_1 = require("./inversify.config");
+dotenv.config();
+require("./inversify.config");
+require("./foo.controller");
+console.log(process.env.PORT);
+// create server
+var server = new inversify_express_utils_1.InversifyExpressServer(inversify_config_1.container);
+server.setConfig(function (app) {
+    // add body parser
+    app.use(bodyParser.urlencoded({
+        extended: true,
+    }));
+    app.use(bodyParser.json());
 });
-app.listen(3000);
+var app = server.build();
+app.listen(process.env.PORT || 8080);
