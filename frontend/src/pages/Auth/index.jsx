b7d4d310dc0e429a7d9e2f5962d54288b805ca3f
KirillNikoda <nikodakirill386@gmail.com>
2022-01-02 18:18:14 +0300

    refactor: move login form in separate module

--- frontend/src/pages/Auth/index.jsx
+++ frontend/src/pages/Auth/index.jsx
@@ -1,85 +1,12 @@
-import { Block } from 'components';
-import { Form, Input, Button, Checkbox } from 'antd';
-import { UserOutlined, LockOutlined } from '@ant-design/icons';
-
-import { useEffect, useState } from 'react';
+import LoginForm from 'modules/LoginForm';
 
 import './Auth.scss';
 
 const Auth = () => {
-  const [, forceUpdate] = useState({});
-
-  useEffect(() => {
-    forceUpdate({});
-  }, []);
-
-  const onFinish = (values) => {
-    console.log('Finish:', values);
-  };
-
   return (
     <section className='auth'>
       <div className='auth__content'>
-        <div className='auth__top'>
-          <h2>Войти в аккаунт</h2>
-          <p>Пожалуйста, войдите в свой аккаунт</p>
-        </div>
-        <Block>
-          <Form
-            name='normal_login'
-            className='login-form'
-            initialValues={{
-              remember: true,
-            }}
-            onFinish={onFinish}
-          >
-            <Form.Item
-              hasFeedback
-              name='username'
-              rules={[
-                {
-                  required: true,
-                  message: 'Пожалуйста, введите имя пользователя!',
-                },
-              ]}
-            >
-              <Input
-                prefix={<UserOutlined className='site-form-item-icon' />}
-                placeholder='Username'
-              />
-            </Form.Item>
-            <Form.Item
-              hasFeedback
-              name='password'
-              rules={[
-                {
-                  required: true,
-                  message: 'Пожалуйста, введите пароль!',
-                },
-              ]}
-            >
-              <Input
-                prefix={<LockOutlined className='site-form-item-icon' />}
-                type='password'
-                placeholder='Password'
-              />
-            </Form.Item>
-
-            <Form.Item>
-              <Button
-                type='primary'
-                htmlType='submit'
-                className='button login-form-button'
-                size='large'
-              >
-                Войти в аккаунт
-              </Button>
-            </Form.Item>
-            <a href='#' className='auth__register-link'>
-              Зарегистрироваться
-            </a>
-          </Form>
-        </Block>
+        <LoginForm />
       </div>
     </section>
   );
